90d4be501733ef1b7b27dd7aad24d52e8432843f
petergourgui <petergourgui@gmail.com>
2024-10-31 00:23:27 -0700

    Updated timer.

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -6,6 +6,10 @@ document.getElementById('nameDisplay').innerText = name;
 // Countdown Timer
 const targetDate = new Date('2024-12-01T14:30:00-08:00').getTime(); // December 1st at 2:30pm
 
+function formatTime(number) {
+    return number < 10 ? '0' + number : number;
+}
+
 function updateTimer() {
     const now = new Date().getTime();
     const distance = targetDate - now;
@@ -16,7 +20,7 @@ function updateTimer() {
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     const timerDisplay = document.getElementById('timer');
-    timerDisplay.textContent = `${days} : ${hours} : ${minutes} : ${seconds}`;
+    timerDisplay.textContent = `${formatTime(days)} : ${formatTime(hours)} : ${formatTime(minutes)} : ${formatTime(seconds)}`;
 
     // If the countdown is over, display a message
     if (distance < 0) {
